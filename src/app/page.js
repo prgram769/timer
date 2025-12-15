16cfedcd2377c30a015b60ec0260d3ff49fb42a0
prgram769 <prgram02@gmail.com>
2025-12-15 20:43:22 +0100

    iniciando el proyecto de timer pero pausandolo de forma temporal

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,9 @@ import { NumberInput } from "@/components/ui/numberInput";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans dark:bg-[#222]">
+      <div>
+        
+      </div>
       <NumberInput typeOfCounter={"Hours"}/>
       <NumberInput typeOfCounter={"Minutes"}/>
       <NumberInput typeOfCounter={"Seconds"}/>

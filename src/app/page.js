552a7a1f0bb8ec00f4a8edb70b11c051c1efd3b4
prgram769 <prgram02@gmail.com>
2025-12-24 00:12:28 +0100

    tamaño aumentado de todo

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import { NumberInput } from "@/components/ui/numberInput";
 export default function Home() {
   return (
     <>
-      <div className="flex min-h-[95.7vh] items-center justify-center font-sans dark:bg-[#222]">
-        <div className="bg-amber-600 rounded px-3 py-2 flex items-center justify-center">
+      <div className="flex min-h-[95.5vh] items-center justify-center font-sans dark:bg-[#222]">
+        <div className="bg-amber-600 h-50 w-150 rounded px-3 py-2 flex items-center justify-center">
           <NumberInput />
         </div>
       </div>
--- src/components/ui/footPage.jsx
+++ src/components/ui/footPage.jsx
@@ -1,7 +1,7 @@
 function FootPage() {
   return (
   	<footer>
-      <p className="font-bold text-black bg-[#D4D4D4] h-10 flex justify-center items-center">
+      <p className="font-bold text-black text-[30px] bg-[#D4D4D4] h-10 flex justify-center items-center">
         Made by rugby01
       </p>
     </footer>
